11a92d98976b6dd1c40668956a21cd773b7fe03f
Aamir Parekh <aamir.parekh36@gmail.com>
2023-09-15 02:07:19 +0500

    Added basic header content
    Will be changed in next commit

--- app.js
+++ app.js
@@ -12,6 +12,31 @@ function PageTransitions(){
             this.className += ' active-btn';
         })
     }
+
+    //Sections Active Class
+
+   for (const sec of allSections){
+   sec.addEventListener('click', (e) =>{
+    const id = e.target.dataset.id;
+    if (id){
+        //remove selected from old button
+        sectBtns.forEach((btn) =>{
+            btn.classList.remove('active')
+        })
+        e.target.classList.add('active')
+
+        //hide other sections
+        sections.forEach((section) =>{
+            section.classList.remove('active')
+        })
+
+        const element = document.getElementById(id);
+        element.classList.add('active');
+    }
+    })}
+
+
+
 }
 
 PageTransitions();
